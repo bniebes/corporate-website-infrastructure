f59921f71b4f303f40ba6fb8c8c885e4fd5acd7e
Brian Niebes <dev.BrianNiebes@gmail.com>
2025-07-08 23:21:04 +0200

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.2.0"
+    }
+  }
+}
+
+# Main region
+
+## EU, Frankfurt
+provider "aws" {
+  alias  = "eu-central-1"
+  region = "eu-central-1"
+}
+
+# Sub regions
+
+## US, Ohio
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+}
+
+## US, Oregon
+provider "aws" {
+  alias  = "us-west-2"
+  region = "us-west-2"
+}
+
+## AP, Mumbai
+provider "aws" {
+  alias  = "ap-south-2"
+  region = "ap-south-2"
+}
+
+## AP, Singapur
+provider "aws" {
+  alias  = "ap-southeast-1"
+  region = "ap-southeast-1"
+}
+
+## AP, Tokyo
+provider "aws" {
+  alias  = "ap-northeast-1"
+  region = "ap-northeast-1"
+}
+
+## AP, Sydney
+provider "aws" {
+  alias  = "ap-southeast-2"
+  region = "ap-southeast-2"
+}
