0f3906ad4a1d845811d06c39d21b7b4026352ebb
Brian Niebes <dev.BrianNiebes@gmail.com>
2025-10-19 23:21:09 +0200

    Add AWS App Runner ressources

--- modules/region-main/main.tf
+++ modules/region-main/main.tf
@@ -19,9 +19,9 @@ data "aws_caller_identity" "current" {}
 
 # AWS ECR #############################################################################################################
 
-resource "aws_ecr_repository" "ecr" {
-  name                 = "ecr_main"
-  image_tag_mutability = "IMMUTABLE"
+resource "aws_ecr_repository" "ecr_main" {
+  name                 = "ecr-main"
+  image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = false
   }
@@ -37,5 +37,70 @@ resource "aws_ecr_replication_configuration" "ecr_main" {
         registry_id = data.aws_caller_identity.current.account_id
       }
     }
+  }
+}
+
+# AWS IAM #############################################################################################################
+
+resource "aws_iam_role" "role_apprunner_ecr" {
+  name = "role-apprunner-ecr"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "build.apprunner.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# AWS App Runner ######################################################################################################
+
+resource "aws_iam_role_policy_attachment" "policy_apprunner_ecr" {
+  role       = aws_iam_role.role_apprunner_ecr.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+resource "time_sleep" "wait_role_create" {
+  depends_on      = [aws_iam_role.role_apprunner_ecr]
+  create_duration = "30s"
+}
+
+resource "aws_apprunner_service" "corporate_website" {
+  service_name = "corporate-website"
+  depends_on   = [time_sleep.wait_role_create]
+
+  source_configuration {
+
+    authentication_configuration {
+      access_role_arn = aws_iam_role.role_apprunner_ecr.arn
+    }
+
+    image_repository {
+      image_identifier      = "${aws_ecr_repository.ecr_main.repository_url}/corporate-website:prod"
+      image_repository_type = "ECR"
+      image_configuration {
+        port = "30123"
+      }
+    }
+
+    auto_deployments_enabled = true
+  }
+
+  network_configuration {
+
+    ingress_configuration {
+      is_publicly_accessible = true
+    }
+  }
+
+  tags = {
+    Name     = "corporate-website-apprunner-service"
+    Image    = "corporate-website"
+    ImageTag = "prod"
   }
 }
--- modules/region-sub/main.tf
+++ modules/region-sub/main.tf
@@ -11,11 +11,76 @@ terraform {
 
 # AWS ECR #############################################################################################################
 
-resource "aws_ecr_repository" "ecr" {
-  name                 = "ecr_sub"
-  image_tag_mutability = "IMMUTABLE"
+resource "aws_ecr_repository" "ecr_sub" {
+  name                 = "ecr-sub"
+  image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = false
   }
   force_delete = false
+}
+
+# AWS IAM #############################################################################################################
+
+resource "aws_iam_role" "role_apprunner_ecr" {
+  name = "role-apprunner-ecr"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "build.apprunner.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# AWS App Runner ######################################################################################################
+
+resource "aws_iam_role_policy_attachment" "policy_apprunner_ecr" {
+  role       = aws_iam_role.role_apprunner_ecr.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+resource "time_sleep" "wait_role_create" {
+  depends_on      = [aws_iam_role.role_apprunner_ecr]
+  create_duration = "30s"
+}
+
+resource "aws_apprunner_service" "corporate_website" {
+  service_name = "corporate-website"
+  depends_on   = [time_sleep.wait_role_create]
+
+  source_configuration {
+
+    authentication_configuration {
+      access_role_arn = aws_iam_role.role_apprunner_ecr.arn
+    }
+
+    image_repository {
+      image_identifier      = "${aws_ecr_repository.ecr_sub.repository_url}/corporate-website:prod"
+      image_repository_type = "ECR"
+      image_configuration {
+        port = "30123"
+      }
+    }
+
+    auto_deployments_enabled = true
+  }
+
+  network_configuration {
+
+    ingress_configuration {
+      is_publicly_accessible = true
+    }
+  }
+
+  tags = {
+    Name     = "corporate-website-apprunner-service"
+    Image    = "corporate-website"
+    ImageTag = "prod"
+  }
 }
